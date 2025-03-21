fa7729d88faa221ea1dae418a9cf8c448f19bef2
Sai Aswin Raja <aswinsai45@gmail.com>
2025-03-21 22:23:34 +0530

    Hero file initiated

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { MdNavigateNext } from "react-icons/md";
 
 function Hero() {
-  return <div></div>;
+  return (
+    <div className="max-w-[1200px] mx-auto">
+      <div>
+        <h1>Albums for you</h1>
+        <img src={<MdNavigateNext />} alt="move right" />
+      </div>
+    </div>
+  );
 }
 export default Hero;
